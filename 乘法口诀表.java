bfcfce475dfda994661f042a28dc749103dc1fc7
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2018-12-03 18:10:51 +0800

    java版乘法口诀表

---
+++ 乘法口诀表.java
@@ -0,0 +1,15 @@
+public class Work1
+{
+    public static void main(String[] args){
+        int i = 0;
+        int j = 0;
+        for(i=1;i<=9;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(i+" "+"*"+" "+j+" "+"="+" "+i*j+"    ");
+            }
+            System.out.println();
+        }
+    }
+}
