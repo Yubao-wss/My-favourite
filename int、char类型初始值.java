309d1490c13b8cc0af1fda28eae1e3785d957378
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2018-12-24 18:37:30 +0800

    int型初始值为0，char型为\u0000(空格）
    创建一个Test类，包含有一个public权限的int型成员变量与一个char类型的成员变量，观察在main方法中的初始值。

---
+++ int、char类型初始值.java
@@ -0,0 +1,11 @@
+public class Day1{
+    public static void main(String[] args){
+        Test test = new Test();
+        System.out.println(test.a);
+        System.out.println(test.b);
+    }
+}
+class Test{
+    public int a;
+    public char b;
+}
