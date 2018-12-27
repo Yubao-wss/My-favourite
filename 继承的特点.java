8d82ed148ceefbe0fcab944fb2e4e55f207355a5
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2018-12-27 17:17:21 +0800

    子类对象在实例化前一定会首先实例化父类对象。 即先调用父类构造，再调子类构造。
    输出结果
    A
    B

---
+++ 继承的特点.java
@@ -0,0 +1,15 @@
+class A{ 
+ public A(){ 
+ System.out.println(“A”); 
+ } 
+ } 
+
+class B extends A{ 
+ public B(){ 
+ System.out.println(“B”); 
+ } 
+
+public static void main(String[] args){ 
+ B b=new B(); 
+ } 
+ } 
